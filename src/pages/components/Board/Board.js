562d79d62d666d8bca9f9ebfa57b267b5739e27a
unknown <pablolope8@gmail.com>
2023-01-22 19:46:02 -0500

    Primer commit

---
+++ src/pages/components/Board/Board.js
@@ -0,0 +1,24 @@
+import MemoBlock from '../MemoBlock/MemoBlock';
+
+const Board = ({
+	animating,
+	handleMemoClick,
+	memoBlocks,
+}) => {
+	return (
+		<main className='board'>
+			{memoBlocks.map((memoBlock, i) => {
+				return (
+					<MemoBlock
+						key={`${i}_${memoBlock.emoji}`}
+						animating={animating}
+						handleMemoClick={handleMemoClick}
+						memoBlock={memoBlock}
+					/>
+				);
+			})}
+		</main>
+	);
+};
+
+export default Board;
